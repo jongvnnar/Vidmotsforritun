dd35e2baf039679aceb69877c767efea63b69f3c
Jón Gunnar Hannesson <jgh060@gmail.com>
2020-04-04 17:17:14 +0000

    Added verkefni6

--- Verkefni5/Calculator.java
+++ Verkefni5/Calculator.java
@@ -1,23 +1,73 @@
+/**
+ * Klasi sem birtir einfalda reiknivél.
+ * Má keyra með því að gera eftirfarandi skipanir:
+ * SET CLASSPATH=.;miglayout-core-5.3-20190920.220054-302.jar;miglayout-swing-5.3-20190918.220057-300.jar
+ * javac -encoding utf8 Calculator.java
+ * java Calculator
+ * fyrir Windows OS.
+ * 
+ * Sé maður að nota LINUX skal keyra
+ * CLASSPATH=.:miglayout-core-5.3-20190920.220054-302.jar:miglayout-swing-5.3-20190918.220057-300.jar
+ * javac -encoding utf8 Calculator.java
+ * java Calculator
+ * 
+ * Reiknivélin styður hvorki svigasetningu né RPN, heldur er nýjasta talan sem reiknuð er geymd og hægt
+ * er að vinna með hana. Reiknisegðir eru metnar með klasanum Evaluate samkvæmt skilum þess klasa, frá 
+ * vinstri til hægri án þess að miða við operator precedence. Reiknivélin styður margföldun, deilingu,
+ * plús og mínus ásamt því að styðja eyðingu og 'backspace'.
+ * 
+ * Til að reikna segðir sem myndu annars krefjast svigasetningar, t.d. '(1+2)*3' má styðja á hnappa
+ *  '1', '+', '2', '=', '*', '3','=' til að fá útkomuna 9. 
+ * 
+ * @author Jón Gunnar Hannesson
+ * @author jgh12@hi.is
+ */
 import net.miginfocom.swing.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.event.*;
 public class Calculator implements ActionListener{
+    /**
+     * currentNum er strengur sem heldur utan um svar síðustu gildu reiknisegðar.
+     * currentNum er grunnstillt sem "0".
+     * expression er strengur sem lýsir reiknisegðinni sem verið er að vinna í
+     * hverju sinni. Grunnstillt sem tómistrengurinn.
+     * frame er JFrame rammi sem inniheldur þá takka sem reiknivélin notar ásamt
+     * tveimur JLabel hlutum lýst neðar. Titlaður "Calculator".
+     * numLabel er JLabel merkjari sem inniheldur currentNum strenginn.
+     * lastOpLabel er JLabel merkjari sem inniheldur expression strenginn.
+     * shouldChange er boolean breyta sem lýsir því hvort ætti að skrifa yfir currentNum
+     * eða hvort ætti að bæta við aftan á currentNum strenginn. Hún er einnig notuð
+     * til að sjá hvort eigi að skrifa yfir expression strenginn eða bæta við hann. 
+     * Grunnstillt sem true.
+     */
     private String currentNum = "0";
     private String expression = "";
     private JFrame frame = new JFrame("Calculator");
     private JLabel numLabel = new JLabel(currentNum);
     private JLabel lastOpLabel = new JLabel(expression);
     private boolean shouldChange = true;
+    /**
+     * Smiður fyrir tilvik af Calculator.
+     * Eftir: Ný reiknivél birtist með breytur grunnstilltar eins og lýst var í 
+     * fastayrðingu gagna.
+     */
     public Calculator(){
         makeUI();
     }
+    /**
+     * Hjálparfall sem byggir reiknivélina innan frame og birtir hana. Reiknivélin er byggð upp sem grid þar sem 
+     * hver sella hefur minnstu stærð sem og preferred size 40x60, nema í fyrstu röð þar sem gildin eru 10x60.
+     * Í hverri röð eru fjórar sellur. Reiknivélin er skalanleg en þó er hægt að skrifa tölur og 
+     * segðir nógu langar til að fara út fyrir rammann. Hægt er að loka reiknivél
+     * með því að ýta á krossinn í efra vinstra horni.
+     */
     private void makeUI(){
         MigLayout layout = new MigLayout("fill, wrap 4", "grow, 60:60:", 
         "[grow, 10:10:] [grow, 40:40:] [grow, 40:40:] [grow, 40:40:][grow, 40:40:][grow, 40:40:][grow, 40:40:]");
         frame.setLayout(layout);
-        // numLabel.setText(currentNum);
+        frame.setMinimumSize(new Dimension(240,250));
         numLabel.setFont(numLabel.getFont().deriveFont(30.0f));
         JButton clear = makeButton("Clear");
         JButton del = makeButton("⌫");
@@ -52,30 +102,68 @@ public class Calculator implements ActionListener{
         frame.pack();
         frame.setVisible(true);
     }
+    /**
+     * Hjálparfall sem skilar JButton takka með actionListener sem bendir á Calculator hlutinn sjálfan.
+     * @param s er strengur sem lýsir titlinum á takkanum
+     * @return JButton með actionListener sem bendir á hlutinn sjálfan með titil s.
+     */
     private JButton makeButton(String s){
         JButton retButton = new JButton(s);
         retButton.addActionListener(this);
         return retButton;
     }
+    /**
+     * Hjálparfall sem gerir manni kleift að eyða aftasta gildi currentNum.
+     * Fyrir: currentNum er strengur af lengd >= 1.
+     * Eftir: ef currentNum var strengur af lengd > 1 er currentNum nú sami 
+     * strengur án aftasta characters.
+     */
     private void backspace(){
         if(currentNum.length() > 1){
             currentNum = currentNum.substring(0, currentNum.length()-1);
         }
     }
+    /**
+     * Hjálparfall við til að styðja clear aðgerðina.
+     * Fyrir: Tilviksbreytur geta verið hvernig sem er.
+     * Eftir: currentNum er "0". expression er
+     * nú tómi strengurinn. shouldChange er nú með gildið true.
+     */
     private void clear(){
         currentNum = "0";
-        if(shouldChange == true)
-            expression = "";
+        expression = "";
         shouldChange = true;
     }
+    /**
+     * Hjálparfall við að bæta aðgerðum í reiknisegð.
+     * @param s strengur sem lýsir aðgerðinni sem á að bæta við.
+     * Eftir: s hefur verið bætt við aftan við expression tilviksbreytuna. shouldChange
+     * er nú stillt sem true.
+     */
     private void addOp(String s){
         expression = expression.concat(currentNum + " " + s + " ");
         shouldChange = true;
     }
+    /**
+     * Hjálparfall við að bæta punktum við tölur til að birta kommutölur.
+     * Fyrir: Tilviksbreytur geta verið hvernig sem er.
+     * Eftir: Punkti hefur verið bætt aftast í currentNum strenginn.
+     * shouldChange er nú stillt sem false.
+     */
     private void addDot(){
         currentNum = currentNum.concat(".");
         shouldChange = false;
     }
+    /**
+     * Hjálparfall við að bæta heiltölum við currentNum.
+     * @param num strengur sem lýsir heiltölu.
+     * Fyrir: currentNum er strengur sem lýsir heiltölu. shouldChange er annaðhvort true
+     *  eða false.
+     * Eftir: Ef shouldChange var true er currentNum nú heiltalan sem bætt var við.
+     *  Ef shouldChange var false er currentNum nú sami strengur að viðbættri með num strenginn
+     *  skeyttan aftan við.
+     *  shouldChange er nú stillt sem false.
+     */
     private void addNum(String num){
         if(shouldChange){
             currentNum = num;
@@ -83,25 +171,42 @@ public class Calculator implements ActionListener{
         else currentNum = currentNum.concat(num);
         shouldChange = false;
     }
+    /**
+     * Hjálparfall sem styður við equals takkann.
+     * Fyrir: expression strengur sem lýsir gildri eða ógildri reiknisegð. currentNum
+     *  er strengur sem lýsir síðustu tölu sem sett var inn í reiknivélina.
+     * Eftir: lastOpLabel sýnir gamla expression strenginn að viðbættu jafnaðarmerki.
+     *  expression er nú tómistrengurinn. Hafi expression lýst gildri reiknisegð hefur currentNum
+     *  verið breytt í svarið við þeirri reiknisegð. Annars er currentNum hið sama og áður og kallað
+     *  hefur verið á errorMessage fall með strengnum "Invalid expression". shouldChange er nú stillt
+     *  sem true
+     */
     private void equals(){
         shouldChange = true;
-        expression = expression.concat(currentNum + " " + "=");
-        lastOpLabel.setText(expression);
-        Evaluate eval;
+        String evalExpression = expression.concat(currentNum + " " + "=");
+        lastOpLabel.setText(evalExpression);
+        Evaluate eval = new Evaluate(evalExpression);
         try{
-        eval = new Evaluate(expression);
         currentNum = eval.getNumber();
         }
         catch(Exception e){
             errorMessage("Invalid Expression");
-            return;
         }
         expression = "";
     }
+    /**
+     * Hjálparfall við að birta villuboð fyrir notanda
+     * @param s er strengur sem inniheldur villuboðin sem birta á
+     */
     private void errorMessage(String s){
         JOptionPane.showMessageDialog(frame, s,
         "Error",JOptionPane.ERROR_MESSAGE);
     }
+    /**
+     * Hjálparfall við að meta hvort strengur lýsi heiltölu eða ekki.
+     * @param num strengur sem lýsir mögulega heiltölu.
+     * @return boolean gildi sem er true ef strengurinn num lýsti heiltölu og false ef ekki.
+     */
     private boolean isNumber(String num){
         try{
             Integer.parseInt(num);
@@ -111,6 +216,10 @@ public class Calculator implements ActionListener{
             return false;
         }
     }
+    /**
+     * Framkvæmir aðgerðirnar lýst að ofan þegar ýtt er á samsvarandi takka.
+     * Uppfærir numLabel og uppfærir lastOpLabel ef ekki var ýtt á jafnaðarmerki.
+     */
     public void actionPerformed(ActionEvent e){
         String actionName = ((JButton)(e.getSource())).getText();
         if(actionName.equals("⌫")) backspace();
@@ -122,8 +231,6 @@ public class Calculator implements ActionListener{
         numLabel.setText(currentNum);
         if(!actionName.equals("="))
             lastOpLabel.setText(expression);
-        frame.pack();
-        frame.setVisible(true);
     }
     public static void main(String[] args){
         Runnable evt = ()->{
--- Verkefni5/Evaluate.java
+++ Verkefni5/Evaluate.java
@@ -1,21 +1,43 @@
+/**
+ * Klasi notaður til að meta gildi reiknisegða. Nota má sem hjálparklasa með því að beita
+ * getNumber() aðferðinni. Klasinn getur metið einfaldar reiknisegðir án sviga með heiltölum og
+ * kommutölum sem beita margföldun, deilingu, plús og mínus. Segðir eru metnar frá vinstri til
+ * hægri, ekki er tekið tillit til operator precedence.
+ * 
+ * @author Jón Gunnar Hannesson
+ * @author jgh12@hi.is
+ */
 public class Evaluate{
-    String[] expression;
-    double retValue;
-    public Evaluate(String expression) throws Exception{
+    /**
+     * Fastayrðing gagna. Expression er fylki strengja, þar sem hvert stak fylkisins á að vera til skiptis
+     * heiltala eða kommutala og aðgerð '+', '-', '*', '/' eða '='.
+     */
+    private String[] expression;
+    /**
+     * Býr til nýtt tilvik af Expression hlut.
+     * @param expression strengur sem lýsir gildri eða ógildri reiknisegð þar sem bil er á milli
+     * talna/aðgerða.
+     * Eftir: tilviksbreytan expression hefur verið grunnstillt sem strengjafylki út frá expression
+     *  parameter þar sem skipt er í bilum.
+     */
+    public Evaluate(String expression){
         this.expression = expression.split(" ");
-        try{
-        retValue = evaluate();
-        }
-        catch(Exception e){
-            throw new Exception("Invalid expression");
-        }
     }
+
+    /**
+     * Hjálparfall sem metur reiknisegðina lýst í tilviksbreytu frá vinstri til hægri
+     * og skilar svari sem double tölu. Kastar villu ef reiknisegðin er ekki gild, þ.e. bilsetning
+     * var ekki rétt í upphafi, notast var við aðgerðir sem Evaluate klasinn skilur ekki eða að 
+     * ekki er skipt á milli talna og aðgerða í hverju staki expression tilviksbreytunnar.
+     * @return double tala sem skilar svari við reiknisegðinni miðað við lýsingu að ofan.
+     * @throws Exception ef reiknisegð er ekki gild eins og lýst er að ofan.
+     */
     private double evaluate() throws Exception{
         double retVal = Double.parseDouble(expression[0]);
         for(int i = 1; i<expression.length-1; i += 2){
             double nextVal;
             try{
-                nextVal =Double.parseDouble(expression[i+1]);
+                nextVal = Double.parseDouble(expression[i+1]);
                 retVal = performOp(retVal, expression[i], nextVal);
             }
             catch(Exception e){
@@ -24,17 +46,40 @@ public class Evaluate{
         }
         return retVal; 
     }
-    private double performOp(double firstNum, String operation, double secondNum){
+    /**
+     * Hjálparfall við að framkvæma reikniaðgerð á tvær double tölur.
+     * @param firstNum fyrra númerið sem á að beita aðgerðinni á
+     * @param operation strengur sem lýsir aðgerðinni sem á að beita.
+     *  Þarf að vera '-','+','*' eða '/'.
+     * @param secondNum seinna númerið sem á að beita aðgerðinni á.
+     * @return double tala sem þar sem beitt hefur verið aðgerðinni operation á tölurnar tvær í röðinni 
+     * firstNum-operation-secondNum.
+     * @throws Exception ef reynt er að beita reikniaðgerðum sem fallið styður ekki
+     */
+    private double performOp(double firstNum, String operation, double secondNum)throws Exception{
         char op = operation.charAt(0);
         switch(op){
             case '/': return firstNum / secondNum;
             case '*': return firstNum * secondNum;
             case '+': return firstNum + secondNum;
             case '-': return firstNum - secondNum;
-            default: return 0.0;
+            default: throw new Exception("Invalid expression");
         }
     }
-    public String getNumber(){
+    /**
+     * Fall sem skilar svari við reiknisegðinni sem sett var í miðað við lýsingar á Evaluate
+     * klasanum að ofan.
+     * @return strengur sem lýsir tölunni sem metin var sem svar, hvort sem það er heiltala eða kommutala.
+     * @throws Exception ef reiknisegðin er ógild eða notast við aðgerðir sem klasinn ekki skilur.
+     */
+    public String getNumber() throws Exception{
+        double retValue;
+        try{
+            retValue = evaluate();
+        }
+        catch(Exception e){
+            throw new Exception("Invalid expression");
+        }
         if(Math.abs(retValue-Math.round(retValue)) < Double.MIN_VALUE) 
             return Long.toString(Math.round(retValue));
         else return Double.toString(retValue);
